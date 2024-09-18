48b9cc055cdc0229b03ec946920771120ef2deba
Bharat <bharat@krishivinnovations.com>
2024-09-18 10:32:10 +0530

    -- build error solved.

--- KHRMS.Infrastructure/Repositories/LeaveRepository.cs
+++ KHRMS.Infrastructure/Repositories/LeaveRepository.cs
@@ -1,15 +1,9 @@
 ﻿using KHRMS.Core.Interfaces;
-using KHRMS.Core.Models;
 using KHRMS.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KHRMS.Infrastructure.Repositories
 {
-     public class LeaveRepository : GenericRepository<LeaveType>, ILeaveRepository
+    public class LeaveRepository : GenericRepository<LeaveType>, ILeaveRepository
     {
         public LeaveRepository(KHRMSContextClass dbContext) : base(dbContext)
         {
--- KHRMS.Services/Interfaces/ILeaveTypeService.cs
+++ KHRMS.Services/Interfaces/ILeaveTypeService.cs
@@ -1,9 +1,4 @@
-﻿using KHRMS.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KHRMS.Core;
 
 namespace KHRMS.Services.Interfaces
 {
