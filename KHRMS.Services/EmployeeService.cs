508a8af97cd41b21438c17a2ab0f4216d787a169
Prem Surani <prem.surani@krishivinnovations.com>
2025-02-11 11:24:20 +0530

    --Assign and update roles of employees

--- KHRMS.Services/EmployeeService.cs
+++ KHRMS.Services/EmployeeService.cs
@@ -1,4 +1,5 @@
 ﻿using KHRMS.Core;
+using KHRMS.Services.Request;
 
 namespace KHRMS.Services
 {
@@ -6,24 +7,46 @@ namespace KHRMS.Services
     {
         public IUnitOfWork _unitOfWork = unitOfWork;
 
-        public async Task<bool> CreateEmployee(Employee employee)
+        public async Task<bool> CreateEmployee(EmployeeRequestModel employeeRequestModel)
         {
-            if (employee != null)
+            if (employeeRequestModel == null)
+                return false;
+
+            var newEmployee = new Employee
             {
-             
-                employee.UpdatedDate = DateTime.Now;
-                await _unitOfWork.Employees.Add(employee);
+                EmployeeCode = employeeRequestModel.EmployeeCode,
+                FirstName = employeeRequestModel.FirstName,
+                LastName = employeeRequestModel.LastName,
+                EmailAddress = employeeRequestModel.EmailAddress,
+                MobileNumber = employeeRequestModel.MobileNumber,
+                DesignationId = employeeRequestModel.DesignationId,
+                DateOfJoining = employeeRequestModel.DateOfJoining,
+                Gender = employeeRequestModel.Gender,
+                CurrentAddress = employeeRequestModel.CurrentAddress,
+                PermanentAddress = employeeRequestModel.PermanentAddress,
+                IsActive = employeeRequestModel.IsActive,
+                CreatedDate = DateTime.Now
+            };
+            await _unitOfWork.Employees.Add(newEmployee);
+            var result = _unitOfWork.Save();
+            if (result > 0 && employeeRequestModel.RoleIds?.Any() == true)
+            {
+                var roleMappings = employeeRequestModel.RoleIds.Select(roleId => new EmployeeRoleMapping
+                {
+                    EmployeeId = newEmployee.Id,
+                    RoleId = roleId,
+                    IsActive = true,
+                    CreatedDate = DateTime.Now
+                }).ToList();
 
-                var result = _unitOfWork.Save();
-
-                if (result > 0)
-                    return true;
-                else
-                    return false;
+                foreach (var roleMapping in roleMappings)
+                {
+                    await _unitOfWork.EmployeeRoleMappings.Add(roleMapping);
+                }
+                _unitOfWork.Save();
             }
-            return false;
+            return result > 0;
         }
-
         public async Task<bool> DeleteEmployee(long employeeId)
         {
             if (employeeId > 0)
@@ -45,10 +68,35 @@ namespace KHRMS.Services
             return false;
         }
 
-        public async Task<IEnumerable<Employee>> GetAllEmployees()
+        public async Task<IEnumerable<EmployeesDTO>> GetAllEmployees()
         {
             var employees = await _unitOfWork.Employees.GetAll();
-            return employees;
+            var employeeroleMapping = await _unitOfWork.EmployeeRoleMappings.GetAll();
+            var rolemaster = (await _unitOfWork.RoleMaster.GetAll()).ToDictionary(role => role.Id);
+
+            var employeesWithRoles = employees.Select(emp => new EmployeesDTO
+            {
+                Id = emp.Id,
+                FirstName = emp.FirstName,
+                LastName = emp.LastName,
+                EmailAddress = emp.EmailAddress,
+                EmployeeCode = emp.EmployeeCode,
+                MobileNumber = emp.MobileNumber,
+                DesignationId = emp.DesignationId,
+                DateOfJoining = emp.DateOfJoining,
+                Gender = emp.Gender,
+                CurrentAddress = emp.CurrentAddress,
+                PermanentAddress = emp.PermanentAddress,
+                IsActive = emp.IsActive,
+                CreatedDate = emp.CreatedDate,
+                RolesName = employeeroleMapping
+           .Where(mapping => mapping.EmployeeId == emp.Id && rolemaster.ContainsKey(mapping.RoleId) && mapping.IsActive)
+           .Select(mapping => rolemaster[mapping.RoleId].RoleName ?? string.Empty)
+           .Where(roleName => !string.IsNullOrEmpty(roleName))
+           .ToList()
+            }).ToList();
+
+            return employeesWithRoles;
         }
 
         public async Task<Employee> GetEmployeeById(int employeeId)
@@ -64,34 +112,64 @@ namespace KHRMS.Services
             return null;
         }
 
-        public async Task<bool> UpdateEmployee(Employee employee)
+        public async Task<bool> UpdateEmployee(EmployeeRequestModel employeeRequestModel)
         {
-            if (employee != null)
-            {
-                var employeeDetails = await _unitOfWork.Employees.GetById(employee.Id);
-                if (employeeDetails != null)
-                {
-                    employeeDetails.EmployeeCode=employee.EmployeeCode;
-                    employeeDetails.FirstName = employee.FirstName;
-                    employeeDetails.LastName = employee.LastName;
-                    employeeDetails.EmailAddress = employee.EmailAddress;
-                    employeeDetails.MobileNumber = employee.MobileNumber;
-                    employeeDetails.DesignationId= employee.DesignationId;
-                    employeeDetails.DateOfJoining= employee.DateOfJoining;
-                    employeeDetails.Gender= employee.Gender;
-                    employeeDetails.CurrentAddress= employee.CurrentAddress;
-                    employeeDetails.PermanentAddress= employee.PermanentAddress;
-                    employeeDetails.UpdatedDate = DateTime.Now;
+            if (employeeRequestModel == null)
+                return false;
 
-                    _unitOfWork.Employees.Update(employeeDetails);
-                    var result = _unitOfWork.Save();
-                    if (result > 0)
-                        return true;
-                    else
-                        return false;
+            var employeeDetails = await _unitOfWork.Employees.GetById(employeeRequestModel.Id);
+            if (employeeDetails == null)
+                return false;
+
+            // Update Employee details
+            employeeDetails.EmployeeCode = employeeRequestModel.EmployeeCode;
+            employeeDetails.FirstName = employeeRequestModel.FirstName;
+            employeeDetails.LastName = employeeRequestModel.LastName;
+            employeeDetails.EmailAddress = employeeRequestModel.EmailAddress;
+            employeeDetails.MobileNumber = employeeRequestModel.MobileNumber;
+            employeeDetails.DesignationId = employeeRequestModel.DesignationId;
+            employeeDetails.DateOfJoining = employeeRequestModel.DateOfJoining;
+            employeeDetails.Gender = employeeRequestModel.Gender;
+            employeeDetails.CurrentAddress = employeeRequestModel.CurrentAddress;
+            employeeDetails.PermanentAddress = employeeRequestModel.PermanentAddress;
+            employeeDetails.IsActive = employeeRequestModel.IsActive;
+            employeeDetails.UpdatedDate = DateTime.Now;
+            _unitOfWork.Employees.Update(employeeDetails);
+            var saveEmployeeResult = _unitOfWork.Save();
+
+            var existingRoleMappings = (await _unitOfWork.EmployeeRoleMappings.GetAll())
+                .Where(r => r.EmployeeId == employeeDetails.Id)
+                .ToList();
+            foreach (var roleMapping in existingRoleMappings.Where(r => r.IsActive))
+            {
+                 roleMapping.IsActive = false;
+                roleMapping.UpdatedDate = DateTime.Now;
+                _unitOfWork.EmployeeRoleMappings.Update(roleMapping);
+            }
+
+            foreach (var roleId in employeeRequestModel.RoleIds)
+            {
+                var existingMapping = existingRoleMappings.FirstOrDefault(r => r.RoleId == roleId);
+
+                if (existingMapping != null)
+                {
+                    existingMapping.IsActive = true;
+                    existingMapping.UpdatedDate = DateTime.Now;
+                    _unitOfWork.EmployeeRoleMappings.Update(existingMapping);
+                }
+                else
+                {
+                    await _unitOfWork.EmployeeRoleMappings.Add(new EmployeeRoleMapping
+                    {
+                        RoleId = roleId,
+                        EmployeeId = employeeDetails.Id,
+                        IsActive = true,
+                        CreatedDate = DateTime.Now
+                    });
                 }
             }
-            return false;
+            var saveRoleMappingsResult = _unitOfWork.Save();
+            return saveEmployeeResult > 0 && saveRoleMappingsResult > 0;
         }
     }
 }
